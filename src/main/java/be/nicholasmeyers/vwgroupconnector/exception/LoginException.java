249aa6fbf0331a62a4bb8ea57c38e0be74611258
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-11-14 00:28:02 +0100

    feat: add an exception with an error message.

---
+++ src/main/java/be/nicholasmeyers/vwgroupconnector/exception/LoginException.java
@@ -0,0 +1,7 @@
+package be.nicholasmeyers.vwgroupconnector.exception;
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) {
+        super(message);
+    }
+}
